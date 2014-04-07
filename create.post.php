bc9ae64a59afad369be8b44ad770883770be084f
Yash Sharma <yashsharma205@gmail.com>
2014-04-07 14:27:03 +0530

    final commit

--- create.post.php
+++ create.post.php
@@ -3,21 +3,31 @@
 // Connect to db
 require 'engine/connection.php';
 
+// Checks for login
+if(!is_logged_in() ){
+	header('Location: /');
+	die();
+}
+
 if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 
-	$title = $_POST['title'];
-	$body = $_POST['body'];
-	$name = $_POST['name'];
+	$title    = trim(htmlentities($_POST['title']));
+	$body     = $_POST['body'];
+	$name     = trim(htmlentities($_POST['name']));
+	$email    = trim(htmlentities($_POST['email']));
+	$category = trim(htmlentities($_POST['category']));
 
 	if( empty($title) || empty($body) || empty($name)) {
 		$status = 'Please fill out all Fields.';
 	} else {
 		query(
-			"INSERT INTO posts(title, name, body, created_at) VALUES(:title, :name, :body, CURRENT_DATE())",
-			array(	'title' => $title, 
-					'name'  => $name, 
-					'body'  => $body),
-							   $conn);
+			"INSERT INTO posts(title, name, email, body, category, created_at) VALUES(:title, :name, :email,:body, :category,CURRENT_DATE())",
+			array(	'title'		=> $title, 
+					'name'  	=> $name, 
+					'email' 	=> $email,
+					'body'  	=> $body,
+					'category'  => $category),
+							   		   $conn);
 		$status = header('Location:index.php');
 	}
 }
@@ -26,37 +36,41 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 <!DOCTYPE HTML>
 <html>
   	<head>
-		<?php head('New Post') ?>
+		<?= head('New Post') ?>
 	</head>
 <body>
-	<div class="container">
-		<form action="" id="form" class="pure-form pure-form-stacked" method="POST">
-			<fieldset>
-				<legend>Create A New Post</legend>
-			  	<label for="name">Full Name :</label>
-				<input type="text" class="pure-input-1" name="name" id="name" placeholder="Enter your name" autofocus required/>
-			
-				<label for="title">Title :</label>
-				<input type="text" class="pure-input-1" name="title" id="title" placeholder="Enter title for your blogpost" required/>
+	<div class="pure-g" id="layout">
+		<?php include 'includes/header.inc.php'; ?>
+		<div class="content pure-u-1 pure-u-med-3-4">
+			<div class="container">
+				<form action="" id="form" class="pure-form pure-form-stacked" method="POST">
+					<fieldset>
+						<legend>Create A New Post</legend>
+					  	<label for="name">Full Name :</label>
+						<input type="text" class="pure-input-1" name="name" id="name" placeholder="Enter your name" autofocus required>
 
-				<label for="body">Body :</label>
-				<textarea name="body" class="pure-input-1" id="body" placeholder="Enter your post description" required></textarea>
+						<label for="email">Email :</label>
+						<input type="email" class"pure-input-1" name="email" id="email" pattern="[^ @]*@[^ @]*\.[a-zA-z]{2,}" placeholder="Enter your email address" required>
+					
+						<label for="title">Title :</label>
+						<input type="text" class="pure-input-1" name="title" id="title" placeholder="Enter title for your blogpost" required>
 
-				<button type="submit" name="submit" class="pure-button">Post</button>
+						<label for="category">Category :</label>
+						<input type="text" class="pure-input-1" name="category" id="category" placeholder="Enter Category Name for your post" required>
 
-				<?php if ( isset($status) ) : ?>
-					<p class="error"><?= $status; ?></p>
-				<?php endif; ?>	
+						<label for="body">Body :</label>
+						<textarea name="body" class="pure-input-1" id="body" placeholder="Enter your post description" required></textarea>
 
-				<footer class="footer">
-					<div class="pure-menu pure-menu-horizontal pure-menu-open">
-						<ul>
-							<li class="dark"><a href="index.php">Go To Homepage</a></li>
-						</ul>	
-					</div>	
-				</footer>	
-			</fieldset>	
-		</form>
-	</div>	
+						<button type="submit" name="submit" class="pure-button">Post</button>
+
+						<?php if ( isset($status) ) : ?>
+							<p class="error"><?= $status; ?></p>
+						<?php endif; ?>	
+					</fieldset>	
+				</form>
+			</div>
+			<?php include 'includes/footer.inc.php'; ?>
+		</div>
+	</div>		
 </body>	
 </html>	
