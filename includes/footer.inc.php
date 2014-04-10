da3b796248cf219c8dc6ac8d8004e7a5b7729310
Yash Sharma <yashsharma205@gmail.com>
2014-04-11 01:42:00 +0530

    Website Footer

---
+++ includes/footer.inc.php
@@ -0,0 +1,17 @@
+<div class="footer">
+	<div class="pure-menu pure-menu-horizontal pure-menu-open">
+		<ul>
+			<li><a href="about.php">About</a></li>
+			<li><a href="http://facebook.com/yasharma">facebook</a></li>
+			<li><a href="https://github.com/yasharma/blog/">Github</a></li>
+			<br>
+			<li class="bottom">&copy; copyright <?= date("Y"); ?> code licensed under the<a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>.</li>
+		</ul>
+	</div>
+</div>
+
+<!-- Javascript -->
+	<script src="js/jquery.js"></script>
+	<script src="js/tabby.js"></script>
+	<!-- js file to beautify code snippets -->
+	<script src="js/prettify.js"></script>
